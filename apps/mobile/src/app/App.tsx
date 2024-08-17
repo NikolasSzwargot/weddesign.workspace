7063fd51428762761ff17334d3f82c4883538b1f
Nikolas Szwargot <nszwargot@gmail.com>
2024-08-18 00:48:24 +0200

    Added navigation

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {WeddesignGlobalStyles, weddesignTheme} from '@weddesign/themes';
 import {ThemeProvider} from 'styled-components/native';
-import {Home} from '@mobile/components';
+import {HomePage} from '@mobile/components';
 import {StatusBar} from 'react-native';
-// import {WeddesignNavigator} from '@mobile/navigation';
+import {NativeRouter, Route, Routes} from "react-router-native";
+import {LogBox} from 'react-native'
+
+//@NOTE: Ignoring that worning, irrevelant to our rc native version
+LogBox.ignoreLogs([
+  'Support for defaultProps will be removed from function components'
+]);
 
 export const App = () => {
     return (
@@ -14,9 +20,11 @@ export const App = () => {
                     backgroundColor={'transparent'}
                     barStyle={'dark-content'}
                 />
-                {/*<WeddesignNavigator />*/}
-                {/*<Home />*/}
-              <Home />
+              <NativeRouter>
+                <Routes>
+                  <Route path={'/'} element={<HomePage />}/>
+                </Routes>
+              </NativeRouter>
             </WeddesignGlobalStyles>
         </ThemeProvider>
     );
--- apps/mobile/src/components/pages/HomePage/index.tsx
+++ apps/mobile/src/components/pages/HomePage/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import {Home} from '@mobile/components';
-import {StackScreenProps} from '@react-navigation/stack';
-import {RootParamList} from '@mobile/navigation';
 
-type HomeProps = StackScreenProps<RootParamList, 'Home'>;
-
-const HomePage = ({navigation, route}: HomeProps) => {
+const HomePage = () => {
     return <Home />;
 };
 
