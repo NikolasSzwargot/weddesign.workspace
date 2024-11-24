bb1340661329845348551107e4943e7094b420a7
JSnotJS <jakubstefanski2002@interia.pl>
2024-11-24 23:50:58 +0100

    ikonki dla kategorii budżetu

--- libs/mobile/assets/src/icons/index.ts
+++ libs/mobile/assets/src/icons/index.ts
@@ -1,5 +1,39 @@
+import Camera from './Camera.svg';
+import Car from './Car.svg';
+import Coffee from './Coffee.svg';
+import Confetti from './Confetti.svg';
+import Decors from './Decors.svg';
+import Dress from './Dress.svg';
+import Earrings from './Earrings.svg';
+import File from './File.svg';
+import FilterDate from './FilterDate.svg';
+import Gift from './Gift.svg';
+import House from './House.svg';
+import Map from './Map.svg';
 import More from './More.svg';
+import Music from './Music.svg';
+import Plus from './Plus.svg';
+import Putus from './Putus.svg';
+import Question from './Question.svg';
+import Rings from './Rings.svg';
 
 export const Icons = {
+    Camera,
+    Car,
+    Coffee,
+    Confetti,
+    Decors,
+    Dress,
+    Earrings,
+    File,
+    FilterDate,
+    Gift,
+    House,
+    Map,
     More,
+    Music,
+    Plus,
+    Putus,
+    Question,
+    Rings,
 };
