bb53f1ba2e8c8e32988abdd8cf5816cd6101ee89
Filip <filip.rosiak02@gmail.com>
2024-11-28 18:47:40 +0100

    dodanie typu guestsGroupedProps

--- apps/mobile/src/api/Guests/useGuestsGrouped.ts
+++ apps/mobile/src/api/Guests/useGuestsGrouped.ts
@@ -4,11 +4,16 @@ import {GuestDto} from '@shared/dto';
 
 import {useUnauthorizedFetch} from '../useUnauthorizedFetch';
 
+type guestsGroupedProps = {
+    title: string;
+    data: GuestDto[];
+};
+
 export const useGuestsGrouped = () => {
     const api = useUnauthorizedFetch();
 
     return useQuery<{title: string; data: GuestDto[]}[], Error>(
         [ApiRoutes.GuestsGrouped],
-        () => api.GET<{title: string; data: GuestDto[]}[]>(ApiRoutes.GuestsGrouped),
+        () => api.GET<guestsGroupedProps[]>(ApiRoutes.GuestsGrouped),
     );
 };
