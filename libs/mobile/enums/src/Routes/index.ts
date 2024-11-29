2a66ed787fea246682026c8d8b91c08ed7910adb
Nikolas Szwargot <nszwargot@gmail.com>
2024-11-29 19:34:13 +0100

    Conflicts resolved

--- libs/mobile/enums/src/Routes/index.ts
+++ libs/mobile/enums/src/Routes/index.ts
@@ -1,9 +1,5 @@
-export enum ApiRoutes {
-    Hello = '/',
-    Login = '/auth/login',
-    Register = '/auth/register',
-}
 export * from './routes';
 export * from './definitions';
 export * from './routes';
 export * from './definitions';
+export * from './apiRoutes';
