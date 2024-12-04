56e430ece5981453170221300809b31b315d5a32
Lena <102764525+Ienia@users.noreply.github.com>
2024-12-04 15:13:01 +0100

    Update useUnauthorizedFetch.ts
    niechcący dodałam xd

--- apps/mobile/src/api/useUnauthorizedFetch.ts
+++ apps/mobile/src/api/useUnauthorizedFetch.ts
@@ -2,6 +2,5 @@ import {fetchWrapper} from '@weddesign/api';
 import {API_URL} from '@env';
 
 export const useUnauthorizedFetch = () => {
-    console.log(API_URL);
     return fetchWrapper(API_URL);
 };
