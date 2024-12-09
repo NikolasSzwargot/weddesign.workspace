f1dd748246f0114687e6c6368ed7c8fa30f33e73
JSnotJS <jakubstefanski2002@interia.pl>
2024-12-10 00:47:53 +0100

    do PRki 2

--- libs/mobile/enums/src/Routes/routes.ts
+++ libs/mobile/enums/src/Routes/routes.ts
@@ -1,4 +1,3 @@
-// ----- GENERAL ROUTES -----
 export enum AppRootRoutes {
     LOGIN = '/login',
     HOME = '/home',
@@ -6,26 +5,22 @@ export enum AppRootRoutes {
     BUDGET = '/budget',
 }
 
-// ----- LOGIN SUBROUTES -----
 export enum LoginRoutes {
     LANGUAGE = '/login/language',
     REGISTER = '/login/register',
     SETUP = '/login/setup',
 }
 
-// ----- HOME SUBROUTES -----
 export enum HomeRoutes {
     HOME = '/home',
 }
 
-// ----- GUEST LIST SUBROUTES -----
 export enum GuestListRoutes {
     GUESTLIST = '/guests',
     LIST = '/guests/list',
     ADD = '/guests/add',
 }
 
-// ----- EXPENSE LIST SUBROUTES -----
 export enum ExpenseListRoutes {
     BUDGET = '/budget',
     LIST = '/budget/list',
