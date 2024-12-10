da344b86c791bc10d8f0e67d316c792a9bca4ecd
Nikolas Szwargot <nszwargot@gmail.com>
2024-12-10 17:25:00 +0100

    KURWA W CHUJ DOJEBANY REGEX

--- libs/mobile/utils/src/lib/isValidEmail.ts
+++ libs/mobile/utils/src/lib/isValidEmail.ts
@@ -6,10 +6,13 @@ export const isValidEmail = (email: string): boolean => {
     //@NOTE: Regex sprawdzający, czy domena zawiera końcówkę, np. .com, .org
     const domainRegex = /\.[a-zA-Z]{2,}$/;
 
+    //@NOTE: Sprawdzenie liczby znaków '@'
+    const atSymbolCount = (email.match(/@/g) || []).length;
+
     //@NOTE: 2 kropki są niedozwolone w adresach email według standardu.
     if (email.includes('..')) {
         return false;
     }
 
-    return emailRegex.test(email) && domainRegex.test(email);
+    return emailRegex.test(email) && domainRegex.test(email) && atSymbolCount === 1;
 };
