82fd4c406659473ad37deae9192266aeacafc8f6
Nikolas Szwargot <nszwargot@gmail.com>
2024-12-10 17:10:39 +0100

    W CHUJ MOCNY REGEX

--- libs/mobile/utils/src/lib/isValidEmail.ts
+++ libs/mobile/utils/src/lib/isValidEmail.ts
@@ -2,9 +2,14 @@ export const isValidEmail = (email: string): boolean => {
     //@NOTE: Regex zgodny ze standardem RFC 5322 do sprawdzania maila
     const emailRegex =
         /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"(\\[\t -~]|[!#-[\]-~ \t])*")@([A-Z0-9]([-A-Z0-9]*[A-Z0-9])?(\.[A-Z0-9]([-A-Z0-9]*[A-Z0-9])?)*)/i;
-    //@NOTE: 2 kropki sa niedozwolone w adresach email według standardu.
+
+    //@NOTE: Regex sprawdzający, czy domena zawiera końcówkę, np. .com, .org
+    const domainRegex = /\.[a-zA-Z]{2,}$/;
+
+    //@NOTE: 2 kropki są niedozwolone w adresach email według standardu.
     if (email.includes('..')) {
         return false;
     }
-    return emailRegex.test(email);
+
+    return emailRegex.test(email) && domainRegex.test(email);
 };
