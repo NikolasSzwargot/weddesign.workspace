44503dac10ce3cbd4270dcf5dae486868a6cdb9a
JSnotJS <jakubstefanski2002@interia.pl>
2024-12-22 16:08:27 +0100

    opcjonalna animacja w podsumowaniu budżetu

--- libs/mobile/components/src/atoms/BudgetFrame/index.tsx
+++ libs/mobile/components/src/atoms/BudgetFrame/index.tsx
@@ -5,67 +5,86 @@ import {Colors} from '@weddesign/enums';
 type BudgetFrameProps = {
     total: number;
     current: number;
-    currency: string;
-    scrollData: Animated.Value;
+    currency?: string;
+    scrollData?: Animated.Value;
 };
 
-const BudgetFrame = ({total, current, currency, scrollData}: BudgetFrameProps) => {
-    const rectangleHeight = scrollData.interpolate({
-        inputRange: [0, 150],
-        outputRange: [120, 40],
-        extrapolate: 'clamp',
-    });
+const BudgetFrame = ({
+    total,
+    current,
+    currency = '',
+    scrollData,
+}: BudgetFrameProps) => {
+    const rectangleHeight = scrollData
+        ? scrollData.interpolate({
+              inputRange: [0, 150],
+              outputRange: [120, 40],
+              extrapolate: 'clamp',
+          })
+        : 40;
 
     const text1Style = {
         transform: [
             {
-                translateX: scrollData.interpolate({
-                    inputRange: [0, 100],
-                    outputRange: [0, -55],
-                    extrapolate: 'clamp',
-                    easing: (x) => 1 - Math.pow(1 - x, 3),
-                }),
+                translateX: scrollData
+                    ? scrollData.interpolate({
+                          inputRange: [0, 100],
+                          outputRange: [0, -55],
+                          extrapolate: 'clamp',
+                          easing: (x) => 1 - Math.pow(1 - x, 3),
+                      })
+                    : -55,
             },
             {
-                translateY: scrollData.interpolate({
-                    inputRange: [0, 100],
-                    outputRange: [-20, 0],
-                    extrapolate: 'clamp',
-                    easing: (x) => x * x * x,
-                }),
+                translateY: scrollData
+                    ? scrollData.interpolate({
+                          inputRange: [0, 100],
+                          outputRange: [-20, 0],
+                          extrapolate: 'clamp',
+                          easing: (x) => x * x * x,
+                      })
+                    : 0,
             },
         ],
     };
-    const fontSize1 = scrollData.interpolate({
-        inputRange: [0, 80],
-        outputRange: [40, 17],
-        extrapolate: 'clamp',
-    });
+    const fontSize1 = scrollData
+        ? scrollData.interpolate({
+              inputRange: [0, 80],
+              outputRange: [40, 17],
+              extrapolate: 'clamp',
+          })
+        : 17;
     const text2Style = {
         transform: [
             {
-                translateX: scrollData.interpolate({
-                    inputRange: [0, 100],
-                    outputRange: [0, 55], // Move right for horizontal layout
-                    extrapolate: 'clamp',
-                    easing: (x) => 1 - Math.pow(1 - x, 3),
-                }),
+                translateX: scrollData
+                    ? scrollData.interpolate({
+                          inputRange: [0, 100],
+                          outputRange: [0, 55], // Move right for horizontal layout
+                          extrapolate: 'clamp',
+                          easing: (x) => 1 - Math.pow(1 - x, 3),
+                      })
+                    : 55,
             },
             {
-                translateY: scrollData.interpolate({
-                    inputRange: [0, 100],
-                    outputRange: [20, 0], // Move down for vertical layout
-                    extrapolate: 'clamp',
-                    easing: (x) => x * x * x,
-                }),
+                translateY: scrollData
+                    ? scrollData.interpolate({
+                          inputRange: [0, 100],
+                          outputRange: [20, 0], // Move down for vertical layout
+                          extrapolate: 'clamp',
+                          easing: (x) => x * x * x,
+                      })
+                    : 0,
             },
         ],
     };
-    const fontSize2 = scrollData.interpolate({
-        inputRange: [0, 80],
-        outputRange: [24, 17],
-        extrapolate: 'clamp',
-    });
+    const fontSize2 = scrollData
+        ? scrollData.interpolate({
+              inputRange: [0, 80],
+              outputRange: [24, 17],
+              extrapolate: 'clamp',
+          })
+        : 17;
 
     return (
         <Animated.View style={[styles.MainView, {height: rectangleHeight}]}>
