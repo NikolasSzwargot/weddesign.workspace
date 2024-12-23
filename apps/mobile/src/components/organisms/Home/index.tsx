76e5bd135f46d3a4e00fb618b2196f27cb70d7af
JSnotJS <jakubstefanski2002@interia.pl>
2024-12-23 14:44:19 +0100

    przechodzenie do listy gości po naciśnięciu przycisku ekranu głównego

--- apps/mobile/src/components/organisms/Home/index.tsx
+++ apps/mobile/src/components/organisms/Home/index.tsx
@@ -5,20 +5,30 @@ import {
     RoundButton,
     TaskFrame,
 } from '@weddesign/components';
-import {Colors} from '@weddesign/enums';
+import {Colors, GuestListRoutes} from '@weddesign/enums';
 import {Text} from '@weddesign/themes';
 import {useTranslation} from 'react-i18next';
 import {getDaysDifference} from '@weddesign/utils';
 import {MockedUser} from '@mobile/mocks';
 
+import {useRouting} from '../../providers';
+
 import {ButtonRow, Container, HomeWrapper, MainFrame} from './styles';
 
 const Home = () => {
     const {t} = useTranslation('home');
+    const {router} = useRouting();
 
     const buttons = [
-        {color: Colors.LightBlue, label: t('buttons.guests')},
-        {color: Colors.LightGreen, label: t('buttons.budget')},
+        {
+            color: Colors.LightBlue,
+            label: t('buttons.guests'),
+            onPress: () => router.navigate(GuestListRoutes.LIST),
+        },
+        {
+            color: Colors.LightGreen,
+            label: t('buttons.budget'),
+        },
         {color: Colors.LightPurple, label: t('buttons.subcontractors')},
         {color: Colors.Yellow, label: t('buttons.tasks')},
     ];
