b7bdd6d7dc1253849ae7fc18d6a3f929397c1049
FilipRosiak1 <filip.rosiak02@gmail.com>
2025-01-02 23:55:09 +0100

    Nawigacja do podwykonawców z ekranu głównego

--- apps/mobile/src/components/organisms/Home/index.tsx
+++ apps/mobile/src/components/organisms/Home/index.tsx
@@ -5,21 +5,28 @@ import {
     RoundButton,
     TaskFrame,
 } from '@weddesign/components';
-import {Colors} from '@weddesign/enums';
+import {Colors, ProvidersRoutes} from '@weddesign/enums';
 import {Text} from '@weddesign/themes';
 import {useTranslation} from 'react-i18next';
 import {getDaysDifference} from '@weddesign/utils';
 import {MockedUser} from '@mobile/mocks';
 
+import {useRouting} from '../../providers';
+
 import {ButtonRow, Container, HomeWrapper, MainFrame} from './styles';
 
 const Home = () => {
     const {t} = useTranslation('home');
+    const {router} = useRouting();
 
     const buttons = [
         {color: Colors.LightBlue, label: t('buttons.guests')},
         {color: Colors.LightGreen, label: t('buttons.budget')},
-        {color: Colors.LightPurple, label: t('buttons.subcontractors')},
+        {
+            color: Colors.LightPurple,
+            label: t('buttons.providers'),
+            onPress: () => router.navigate(ProvidersRoutes.GROUPED),
+        },
         {color: Colors.Yellow, label: t('buttons.tasks')},
     ];
 
