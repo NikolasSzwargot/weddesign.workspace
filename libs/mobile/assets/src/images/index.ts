b187ddb537301eed8ec32e5587c6ef7e38088980
Nikolas Szwargot <nszwargot@gmail.com>
2025-01-08 00:12:46 +0100

    Name and big icon

--- libs/mobile/assets/src/images/index.ts
+++ libs/mobile/assets/src/images/index.ts
@@ -1,9 +1,11 @@
 import Logo from './Logo.svg';
 import Label from './Label.svg';
 import BrokenHeart from './BrokenHeart.svg';
+import AppIcon from './AppIcon.svg';
 
 export const Images = {
     Logo,
     Label,
     BrokenHeart,
+    AppIcon,
 };
